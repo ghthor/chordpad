0de868be10d5ab94fd0a82e10887f931f9a6cb35
Wilhelmina Drengwitz <ghthor@gmail.com>
2017-02-11 01:03:26 -0500

    [chordpad] add touchpad composable zones for chording

--- evdev.go
+++ evdev.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
 	"time"
 
 	"github.com/cenk/backoff"
@@ -11,12 +12,123 @@ import (
 	evdev "github.com/ghthor/golang-evdev"
 )
 
+type ChordIndex uint
+
+func keyDown(m input.Model, i ChordIndex) input.Model {
+	m.Keys |= (1 << i)
+	m.Build |= m.Keys
+	m.Trigger = 0
+	return m
+}
+
+func keyUp(m input.Model, i ChordIndex) input.Model {
+	m.Keys ^= (1 << i)
+	m.Trigger = m.Build
+	m.Build = 0
+	return m
+}
+
+func applyKey(state evdev.KeyEventState) func(input.Model, ChordIndex) input.Model {
+	switch state {
+	case evdev.KeyDown:
+		return keyDown
+	default:
+		return keyUp
+	}
+}
+
+const (
+	PAD_S ChordIndex = iota
+	PAD_E
+	PAD_N
+	PAD_W
+)
+
+const PAD_ALL input.Chord = 1 | 2 | 4 | 8
+
+const MaxDeadzone = 5 * math.MaxInt16 / 10
+const MaxDeadzoneSq = MaxDeadzone * MaxDeadzone
+
+const (
+	BTN_A ChordIndex = iota + 8
+	BTN_TL1
+	BTN_TL0
+	BTN_THUMBL
+
+	BTN_THUMBR
+	BTN_TR0
+	BTN_TR1
+	BTN_B
+)
+
+var BtnIndex = map[int]ChordIndex{
+	evdev.BTN_A:      BTN_A,
+	evdev.BTN_B:      BTN_B,
+	evdev.BTN_TL:     BTN_TL0,
+	evdev.BTN_TR:     BTN_TR0,
+	evdev.BTN_THUMBL: BTN_THUMBL,
+	evdev.BTN_THUMBR: BTN_THUMBR,
+}
+
+type AbsPad struct {
+	offset ChordIndex
+	x, y   int32
+}
+
+func (p AbsPad) Update(m input.Model) input.Model {
+	if p.y == 0 && p.x == 0 {
+		return p.touchUp(m)
+	}
+	return p.touchMove(m)
+}
+
+func (p AbsPad) touchMove(m input.Model) input.Model {
+	keys := m.Keys | (buttonFor(p.x, p.y) << p.offset)
+	if m.Keys == 0 {
+		m.Build = keys
+	} else if m.Keys != keys {
+		m.Build = keys
+	}
+	m.Keys = keys
+	m.Trigger = 0
+	return m
+}
+
+func (p AbsPad) touchUp(m input.Model) input.Model {
+	m.Keys = m.Keys &^ (PAD_ALL << p.offset)
+	m.Trigger = m.Build
+	m.Build = 0
+	return m
+}
+
+func buttonFor(x, y int32) input.Chord {
+	var xx = x * x
+	var yy = y * y
+	if xx+yy < MaxDeadzoneSq {
+		return 0
+	}
+
+	if xx > yy {
+		if x < 0 {
+			return (1 << PAD_W)
+		} else {
+			return (1 << PAD_E)
+		}
+	} else {
+		if y < 0 {
+			return (1 << PAD_S)
+		} else {
+			return (1 << PAD_N)
+		}
+	}
+}
+
 type AbsTrigger struct {
 	output ChordIndex
 	value  int32
 }
 
-func (t *AbsTrigger) Update(model Model, value int32) Model {
+func (t *AbsTrigger) Update(model input.Model, value int32) input.Model {
 	if t.value < 255 && value < 255 {
 		t.value = value
 		return model
@@ -28,17 +140,13 @@ func (t *AbsTrigger) Update(model Model, value int32) Model {
 
 	t.value = value
 	if value == 255 {
-		return model.keyDown(t.output)
+		return keyDown(model, t.output)
 	}
 
-	return model.keyUp(t.output)
+	return keyUp(model, t.output)
 }
 
-type steamController struct {
-	*evdev.InputDevice
-
-	triggers map[int]*AbsTrigger
-}
+type triggers map[int]*AbsTrigger
 
 func newTriggers() map[int]*AbsTrigger {
 	return map[int]*AbsTrigger{
@@ -47,54 +155,43 @@ func newTriggers() map[int]*AbsTrigger {
 	}
 }
 
-type Model struct {
-	input.Model
+type touchpads struct {
+	left, right *AbsPad
 }
 
-type ChordIndex uint
+func (t touchpads) Update(m input.Model, e evdev.AbsEvent) input.Model {
+	switch e.AxisCode {
+	case evdev.ABS_HAT0X:
+		t.left.x = e.Value
+		return t.left.Update(m)
+	case evdev.ABS_HAT0Y:
+		t.left.y = e.Value
+		return t.left.Update(m)
 
-const (
-	BTN_A ChordIndex = iota
-	BTN_TL1
-	BTN_TL0
-	BTN_THUMBL
+	case evdev.ABS_RX:
+		t.right.x = e.Value
+		return t.right.Update(m)
+	case evdev.ABS_RY:
+		t.right.y = e.Value
+		return t.right.Update(m)
 
-	BTN_THUMBR
-	BTN_TR0
-	BTN_TR1
-	BTN_B
-)
-
-var xboxBtnToChordIndex = map[int]ChordIndex{
-	evdev.BTN_A:      BTN_A,
-	evdev.BTN_B:      BTN_B,
-	evdev.BTN_TL:     BTN_TL0,
-	evdev.BTN_TR:     BTN_TR0,
-	evdev.BTN_THUMBL: BTN_THUMBL,
-	evdev.BTN_THUMBR: BTN_THUMBR,
-}
-
-func (m Model) applyKey(state evdev.KeyEventState) func(ChordIndex) Model {
-	switch state {
-	case evdev.KeyDown:
-		return m.keyDown
 	default:
-		return m.keyUp
+	}
+	return m
+}
+
+func newPadAxes() touchpads {
+	return touchpads{
+		left:  &AbsPad{offset: 0},
+		right: &AbsPad{offset: 4},
 	}
 }
 
-func (m Model) keyDown(i ChordIndex) Model {
-	m.Keys |= (1 << i)
-	m.Build |= m.Keys
-	m.Trigger = 0
-	return m
-}
+type steamController struct {
+	*evdev.InputDevice
 
-func (m Model) keyUp(i ChordIndex) Model {
-	m.Keys ^= (1 << i)
-	m.Trigger = m.Build
-	m.Build = 0
-	return m
+	touchpads
+	triggers
 }
 
 func (dev steamController) Update(model input.Model) (input.Model, error) {
@@ -111,8 +208,8 @@ func (dev steamController) Update(model input.Model) (input.Model, error) {
 	case evdev.EV_KEY:
 		ke := evdev.NewKeyEvent(e)
 
-		if index, exists := xboxBtnToChordIndex[int(ke.Scancode)]; exists {
-			return Model{model}.applyKey(ke.State)(index).Model, nil
+		if index, exists := BtnIndex[int(ke.Scancode)]; exists {
+			return applyKey(ke.State)(model, index), nil
 		}
 
 		// TODO: Bind all possible buttons
@@ -131,8 +228,9 @@ func (dev steamController) Update(model input.Model) (input.Model, error) {
 		case evdev.ABS_Z:
 			fallthrough
 		case evdev.ABS_RZ:
-			return dev.triggers[abs.AxisCode].Update(Model{model}, abs.Value).Model, nil
+			return dev.triggers[abs.AxisCode].Update(model, abs.Value), nil
 		default:
+			return dev.touchpads.Update(model, *abs), nil
 		}
 		return model, nil
 
@@ -201,5 +299,5 @@ func autoSelectEvdevDevice() input.Device {
 	}
 
 	// TODO: Map into different types of devices
-	return steamController{dev, newTriggers()}
+	return steamController{dev, newPadAxes(), newTriggers()}
 }
